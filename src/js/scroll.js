2df75bf65db1a8b442adb4b33f4b811b84d41057
KlymenkoAntony <102675462+KlymenkoAntony@users.noreply.github.com>
2022-09-22 22:47:11 +0300

    Update scroll.js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,8 +1,11 @@
 export function scroll() {
-    const listElm = document.querySelector('.gallery');
-    listElm.addEventListener('scroll', e => {
-      if(listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
-        this.loadMore();
-      }
+    const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
     });
+
 }
